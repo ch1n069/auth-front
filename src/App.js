569ef806c004a479effd38bff1054274127a6814
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-26 11:32:58 +0300

    :paint: created folders for components and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <p>here is react site</p>
+    </div>
+  );
 }
 
 export default App;
