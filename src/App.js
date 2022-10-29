c4baeb11d7077e42bfe1c6e644557658771c7fa8
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-29 10:26:39 +0300

    :bug: fixed routing bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route element={<HomePage />} path="/" exact />
-        <Route element={<LoginPage />} path="/login" />
+        <Routes>
+          <Route element={<HomePage />} path="/" exact />
+          <Route element={<LoginPage />} path="/login" />
+        </Routes>
       </Router>
 
       {/* <HomePage /> */}
