3e30bba435d765789f6fa43dee9975a0294c5cc7
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-31 12:34:56 +0300

    created user login function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,7 +6,21 @@ export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
   let [user, setUser] = useState(null);
-  let [authToken, setAuthToken] = useState(null);
-  let contextData = {};
-  return <AuthContext.Provider value="">{children}</AuthContext.Provider>;
+  let [authTokens, setAuthTokens] = useState(null);
+
+  //   function to login user
+  let loginUser = async (e) => {
+    let response = fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ username: null, password: null }),
+    });
+  };
+  let contextData = {
+    loginUser: loginUser,
+  };
+
+  return (
+    <AuthContext.provider value={contextData}>{children}</AuthContext.provider>
+  );
 };
